ef0a152a11b8e4db489bacd0b942af1de8a68a32
wsking233 <wsking233@gmail.com>
2023-05-16 12:20:22 +1200

    Update homelayout.tsx

--- front-end/src/pages/homelayout.tsx
+++ front-end/src/pages/homelayout.tsx
@@ -164,52 +164,13 @@ function DashboardContent() {
                         overflow: 'auto',
                     }} >
                     <Toolbar />
-                    {/* <Container className='container'>
+                    <Container className='container'>
                         here to put the main content
                         <Outlet></Outlet>
+                        {mainDashboard}
                     </Container>
-                     */}
-                         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={6}>
-            Teams:
-                <Paper
-                    sx={{
-                        p: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        height: 240,
-                    }}
-                >
-                    {/* <Chart /> */}
-                    <p>Show Teams Infor here</p>
-                </Paper>
-            </Grid>
-            <Grid item xs={12} md={6} lg={6}>
-            Projects:
-                <Paper
-                    sx={{
-                        p: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        height: 240,
-                    }}
-                >
-                    {/* <Deposits /> */}
-                     <p>Show Projects Infor</p>
-                </Paper>
-            </Grid>
-            <Grid item xs={12}>
-            Students:
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                    {/* <Orders /> */}
-                    <p>Show Studnets Infor</p>
-                </Paper>
-            </Grid>
-        </Grid>
-        {/* <Copyright sx={{ pt: 4 }} /> */}
-    </Container>
-
+                    
+                         
                 </Box>
 
             </Box>
